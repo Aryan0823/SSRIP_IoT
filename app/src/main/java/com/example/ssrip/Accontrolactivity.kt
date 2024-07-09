a5c87539d466b35bc203abfc883b84b063aec4af
Diya-Chauhan <98373841+Diya-Chauhan@users.noreply.github.com>
2024-07-09 12:46:17 +0530

    Ac control

---
+++ app/src/main/java/com/example/ssrip/Accontrolactivity.kt
@@ -0,0 +1,20 @@
+package com.example.ssrip
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Accontrolactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_accontrolactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainButton)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
