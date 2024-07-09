c58b64022407d9a73efb7e0684d21c8ab107a4f4
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2024-07-09 22:12:13 +0530

    Commit Blank Activity AcControlActivity FanControlActivity HumidifierControlActivity LightControlActivity

--- app/src/main/java/com/example/ssrip/AcControlActivity.kt
+++ app/src/main/java/com/example/ssrip/AcControlActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Accontrolactivity : AppCompatActivity() {
+class AcControlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_accontrolactivity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainButton)) { v, insets ->
+        setContentView(R.layout.activity_ac_control)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
